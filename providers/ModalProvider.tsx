9fd8e826e82b994706e3eade2cf73c699948c08c
Alkinoos Sarioglou <alksarioglou@gmail.com>
2024-04-07 19:53:58 +0200

    auth done

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+// package imports
+import { useEffect, useState } from "react";
+
+// custom imports
+import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+
+const ModalProvider = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <>
+            <AuthModal />
+            <UploadModal />
+        </>
+    );
+
+}
+
+export default ModalProvider;
